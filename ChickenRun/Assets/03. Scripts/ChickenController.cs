cb0ffbb41562149a1b02deab8bd041c5b81f2824
hee103 <gmlgml3213@gmail.com>
2025-08-07 20:33:58 +0900

    Wip

--- ChickenRun/Assets/03. Scripts/ChickenController.cs
+++ ChickenRun/Assets/03. Scripts/ChickenController.cs
@@ -19,7 +19,6 @@ public class ChickenController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
        Cursor.lockState = CursorLockMode.Locked;
     }
     void FixedUpdate()
