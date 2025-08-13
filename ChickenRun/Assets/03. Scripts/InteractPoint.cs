d3a3e30846b23a3f4743d9774ce8e19919e4a819
hee103 <gmlgml3213@gmail.com>
2025-08-13 22:31:54 +0900

    Wip
    아이템 데이터와 UI 먼저 구현하기

--- ChickenRun/Assets/03. Scripts/InteractPoint.cs
+++ ChickenRun/Assets/03. Scripts/InteractPoint.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class InteractPoint : MonoBehaviour
 {
+    public float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+
+    public GameObject curInteractGameObject;
+
+    private Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width/2,Screen.height/2));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, maxCheckDistance,layerMask))
+        {
+            if(hit.collider.gameObject != curInteractGameObject)
+            {
+                curInteractGameObject = hit.collider.gameObject;
+                
+            }
+        }
+        else
+        {
+
+        }
     }
 }
